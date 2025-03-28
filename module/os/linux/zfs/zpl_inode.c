cc5bc6eb433f48dc80b7a49ddefc8b54b7a0baf1
Teemu Grönqvist <teemu.gronqvist@net9.fi>
2025-03-28 04:56:30 +0100

    Add FIEMAP support
        The FIEMAP ioctl is the standard Linux user space mechanism for
        inspecting physical layout of a file on disk.  The ioctl returns
        a list of extents each of which describes a region of the file.
        Compatible blocks are merged in to larger extents when they are
        physically contiguous and have identical flags.
    
        The following per-extent flags are supported by ZFS.
    
          FIEMAP_EXTENT_LAST           - The last extent in the mapping
          FIEMAP_EXTENT_UNKNOWN        - Set on gang blocks
          FIEMAP_EXTENT_DELALLOC       - Dirty extents not yet written
          FIEMAP_EXTENT_ENCODED        - Set on compressed extents
          FIEMAP_EXTENT_DATA_ENCRYPTED - Set on encrypted extents
          FIEMAP_EXTENT_NOT_ALIGNED    - Extent is not block aligned
          FIEMAP_EXTENT_DATA_INLINE    - Set on embedded block pointers
          FIEMAP_EXTENT_UNWRITTEN      - Set on holes (normally not reported)
          FIEMAP_EXTENT_MERGED         - Multiple block pointers were merged
          FIEMAP_EXTENT_SHARED         - Set on deduplicated extents
    
        The following flags are supported when requesting extents.  Note
        that *_COPIES, *_NOMERGE, and *_HOLE are currently specific to
        ZFS but are applicable to other Linux file systems.
    
          FIEMAP_FLAG_SYNC             - Sync the file first
          FIEMAP_FLAG_COPIES           - Include all copies of an extent
          FIEMAP_FLAG_NOMERGE          - Don't merge blocks in to extents
          FIEMAP_FLAG_HOLES            - Include unwritten holes as an extent
    
        Finally, the following reserved fields in the fiemap_extent structure
        have been utilized and should be reserved to prevent future conflicts.
    
          .fe_reserved[0]   - Unique device ID; top-level VDEV ID for ZFS
          .fe_reserved64[0] - Physical length of an extent
    
        Future work:
    
        * The FIEMAP_FLAG_XATTR flag is not supported.  Implementing this
          should be relatively straight forward if it is needed.  This would
          be a handy way to determine if the xattrs have been stored in the
          dnode, spill block, or an external object.
    
        * The FIEMAP_FLAG_CACHE flag is not supported.  We rely on the ARC
          to keep the right blocks cached.
    
        * The lseek(2) SEEK_HOLE and SEEK_DATA flags still rely on the
          dmu_offset_next() function.  The zpl_llseek() function can be
          updated to use FIEMAP to quickly determine the next extent.
          This has the advantage of correctly accounting for newly dirtied
          or freed blocks without forcing a txg sync.
    
        The FIEMAP interface is fully described at:
    
        * https://www.kernel.org/doc/Documentation/filesystems/fiemap.txt
    
        Signed-off-by: Brian Behlendorf <behlendorf1@llnl.gov>
        Issue #264

--- module/os/linux/zfs/zpl_inode.c
+++ module/os/linux/zfs/zpl_inode.c
@@ -799,6 +799,44 @@ out:
 	return (error);
 }
 
+/*
+ * Valid FIEMAP flags:
+ * - FIEMAP_FLAG_SYNC    - Sync extents before reporting
+ * - FIEMAP_FLAG_XATTR   - Report extents used by xattrs (Unsupported)
+ * - FIEMAP_FLAG_COPIES  - Report all data copies (ZFS-only)
+ * - FIEMAP_FLAG_NOMERGE - Never merge blocks in to extents (ZFS-only)
+ */
+static int
+zpl_fiemap(struct inode *ip, struct fiemap_extent_info *fei,
+    __u64 start, __u64 len)
+{
+	zfs_fiemap_t *fm;
+	unsigned int flags = fei->fi_flags;
+	fstrans_cookie_t cookie;
+	int error = 0;
+
+	/* Incompatible ZFS-only flags masked out of compatibility check */
+	fei->fi_flags &= ~ZFS_FIEMAP_FLAGS_ZFS;
+
+	error = fiemap_prep(ip, fei, start, &len, ZFS_FIEMAP_FLAGS_COMPAT);
+	if (error)
+		return (error);
+
+	fm = zfs_fiemap_create(start, len, flags, fei->fi_extents_max);
+
+	cookie = spl_fstrans_mark();
+	error = -zfs_fiemap_assemble(ip, fm);
+	spl_fstrans_unmark(cookie);
+
+	if (error)
+		return (error);
+
+	error = -zfs_fiemap_fill(fm, fei, start, len);
+	zfs_fiemap_destroy(fm);
+
+	return (error);
+}
+
 const struct inode_operations zpl_inode_operations = {
 	.setattr	= zpl_setattr,
 	.getattr	= zpl_getattr,
@@ -811,6 +849,7 @@ const struct inode_operations zpl_inode_operations = {
 	.get_acl	= zpl_get_acl,
 #endif /* HAVE_GET_INODE_ACL */
 #endif /* CONFIG_FS_POSIX_ACL */
+	.fiemap         = zpl_fiemap,
 };
 
 const struct inode_operations zpl_dir_inode_operations = {
@@ -841,6 +880,11 @@ const struct inode_operations zpl_dir_inode_operations = {
 	.get_acl	= zpl_get_acl,
 #endif /* HAVE_GET_INODE_ACL */
 #endif /* CONFIG_FS_POSIX_ACL */
+#ifdef HAVE_RENAME2_OPERATIONS_WRAPPER
+	},
+	.rename2	= zpl_rename2,
+#endif
+	.fiemap         = zpl_fiemap,
 };
 
 const struct inode_operations zpl_symlink_inode_operations = {
@@ -848,6 +892,7 @@ const struct inode_operations zpl_symlink_inode_operations = {
 	.setattr	= zpl_setattr,
 	.getattr	= zpl_getattr,
 	.listxattr	= zpl_xattr_list,
+	.fiemap         = zpl_fiemap,
 };
 
 const struct inode_operations zpl_special_inode_operations = {
